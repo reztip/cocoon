644ec8984c4765f5728427f90827f51462c2039a
Billy Le <lebilly87@gmail.com>
2018-11-03 13:04:53 -0700

    added hasura to gatsby-config

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Image from '../components/image'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -12,8 +12,27 @@ const IndexPage = () => (
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
+    {data.cocoon.users.map(user => (
+      <>
+        <p>{`${user.first_name} ${user.last_name}`}</p>
+        <p>email: {user.email}</p>
+      </>
+    ))}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexPageQuery {
+    cocoon {
+      users {
+        id
+        first_name
+        last_name
+        email
+      }
+    }
+  }
+`
