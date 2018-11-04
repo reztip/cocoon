893726c668ef941c65303bea83e7cc3bd0d9276e
Alex Pitzer <apitzer3@gatech.edu>
2018-11-04 11:43:14 -0800

    empty file

---
+++ src/components/Media.js
@@ -0,0 +1,16 @@
+class Media {
+
+    constructor(value){
+        this.value = value || null;
+        this.id = null;
+    }
+
+    setId(id){
+        this.id = id;
+    }
+
+    setValue(value){
+        this.value = value;
+    }
+
+}
